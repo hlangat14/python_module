50074789510451582299468e475de2f23226ad72
unknown <hlangat2011@gmail.com>
2024-03-03 12:12:54 +0300

    first commit

---
+++ Hello/assign.py
@@ -0,0 +1,5 @@
+site_name = "Power Learn Project"
+print(site_name)
+
+site_name = "Apple.com"
+print(site_name)
---
+++ user_input.py
@@ -0,0 +1,4 @@
+Name = input("What is your name?")
+Age = int(input("How old are you?"))
+Location = input("What is your location?")
+print(f' Hello {Name} you are {Age} years old and your location is {Location}')
